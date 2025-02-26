71164aa830b96ecd3d986c43f24188eccb542313
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2025-02-26 19:57:10 -0300

    feat: added menu

--- src/main/java/com/pedroalonso/software_eng/three_layers/main/Main.java
+++ src/main/java/com/pedroalonso/software_eng/three_layers/main/Main.java
@@ -7,9 +7,10 @@ package com.pedroalonso.software_eng.three_layers.main;
 import com.pedroalonso.software_eng.three_layers.controllers.PersonController;
 import com.pedroalonso.software_eng.three_layers.entities.Person;
 import com.pedroalonso.software_eng.three_layers.utils.Utils;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.UUID;
 
 /**
  *
@@ -35,10 +36,86 @@ public class Main {
         program.startProgram();
     }
 
-    private static synchronized void startProgram() {
+    private void startProgram() {
         ArrayList<Person> currentDatabase = personController.getAll();
         if (currentDatabase.isEmpty()) {
             Utils.seedDatabase();
         }
+        System.out.println("====== BEM-VINDO ======");
+        menu();
+    }
+
+    private void menu() {
+        int option;
+        String menuOptions = ("""
+                                [1] Listar usuários
+                                [2] Criar usuário
+                                [3] Buscar usuário [em breve]
+                                [0] SAIR
+                               """);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("ESCOLHA SUA AÇÃO");
+        System.out.println(menuOptions);
+        try {
+            option = Integer.parseInt(reader.readLine());
+            runOption(option);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listUsers() {
+        ArrayList<Person> dataset = personController.getAll();
+        System.out.println("== LISTANDO USUÁRIOS ==");
+        for (Person p : dataset) {
+            String s = String.format("%s - %s - %s - %s", p.getDocument(), p.getName(), p.getEmail(), p.getBirthDate());
+            System.out.println(s);
+        }
+    }
+
+    private void createUser() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name, email, document, birthDateString;
+        LocalDate birthDate;
+        System.out.println("== CRIANDO USUÁRIO ==");
+        try {
+            System.out.println("Nome: ");
+            name = reader.readLine();
+            System.out.println("E-mail: ");
+            email = reader.readLine();
+            System.out.println("CPF: ");
+            document = reader.readLine();
+            System.out.println("Data de nascimento (yyyy-mm-dd): ");
+            birthDateString = reader.readLine();
+            if (!Utils.isLocalDateStringValid(birthDateString)) {
+                System.out.println("!!! DADOS INVÁLIDOS !!!");
+                menu();
+                return;
+            }
+            birthDate = Utils.getDateFromString(birthDateString);
+            Person p = new Person(name, birthDate, email, document);
+            personController.create(p);
+            System.out.println("USUÁRIO CRIADO COM SUCESSO!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void runOption(int option) {
+
+        switch (option) {
+            case 0:
+                System.exit(option);
+            case 1:
+                listUsers();
+                break;
+            case 2:
+                createUser();
+                break;
+            default:
+                System.out.println("OPÇÃO INVÁLIDA");
+                break;
+        }
+        menu();
     }
 }
--- src/main/java/com/pedroalonso/software_eng/three_layers/utils/Utils.java
+++ src/main/java/com/pedroalonso/software_eng/three_layers/utils/Utils.java
@@ -7,6 +7,7 @@ package com.pedroalonso.software_eng.three_layers.utils;
 import com.pedroalonso.software_eng.three_layers.controllers.PersonController;
 import com.pedroalonso.software_eng.three_layers.entities.Person;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -61,6 +62,88 @@ public class Utils {
             Person p = new Person(names[i], u.randomDateGenerator(), email, documents[i]);
             personController.create(p);
         }
+    }
+
+    private static int[] getNumbersFromLocalDateString(String dateString) {
+        String[] splittedDate = dateString.split("-");
+        String yearString,
+                monthString,
+                dayString;
+        int year,
+                month,
+                day;
+        int[] dateNumbers = new int[3];
+        yearString = splittedDate[0];
+        monthString = splittedDate[1];
+        dayString = splittedDate[2];
+        year = Integer.parseInt(yearString);
+        month = Integer.parseInt(monthString);
+        day = Integer.parseInt(dayString);
+        dateNumbers[0] = year;
+        dateNumbers[1] = month;
+        dateNumbers[2] = day;
+        return dateNumbers;
+    }
+
+    public static boolean isLocalDateStringValid(String dateString) {
+        LocalDate date,
+                today = LocalDate.now();
+        YearMonth yearMonth;
+        int maximumYear = today.getYear() - 18,
+                minimumYear = LocalDate.MIN.getYear(),
+                year,
+                month,
+                day;
+        int[] dateNumbers = new int[3];
+        String[] splittedDate;
+        if (dateString.length() != 10) {
+            System.out.println("TAMANHO INVÁLIDO");
+            return false;
+        }
+        splittedDate = dateString.split("-");
+        if (splittedDate[0].length() != 4
+                || splittedDate[1].length() != 2
+                || splittedDate[2].length() != 2) {
+
+            System.out.println("DATAS INVÁLIDAS");
+            return false;
+        }
+        dateNumbers = getNumbersFromLocalDateString(dateString);
+        year = dateNumbers[0];
+        month = dateNumbers[1];
+        day = dateNumbers[2];
+        if (year < minimumYear
+                || year > maximumYear
+                || month < 1
+                || month > 12) {
+
+            System.out.println("INTERVALO INVÁLIDO");
+            return false;
+        }
+        yearMonth = YearMonth.of(year, month);
+        if (!yearMonth.isValidDay(day)) {
+            System.out.println(String.format("DIA: %s, MES: %s, ANO: %s", day, month, year));
+            System.out.println("DIA INVÁLIDO");
+            return false;
+        }
+        return true;
+    }
+
+    public static LocalDate getDateFromString(String dateString) {
+        int[] dateNumbers = new int[3];
+        int year,
+                month,
+                day;
+        LocalDate date;
+        if (!isLocalDateStringValid(dateString)) {
+            return null;
+        }
+        dateNumbers = getNumbersFromLocalDateString(dateString);
+        year = dateNumbers[0];
+        month = dateNumbers[1];
+        day = dateNumbers[2];
+        date = LocalDate.of(year, month, day);
+        return date;
     }
 
     private LocalDate randomDateGenerator() {
